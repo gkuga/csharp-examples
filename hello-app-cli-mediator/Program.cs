7de7bf2795d4007b0fdb667f67c2c53fc2bee8fd
gkuga <yasui_gaku@cyberagent.co.jp>
2022-05-12 01:12:35 +0900

    Add mediator

---
+++ hello-app-cli-mediator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace hello_app_cli_mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greeterA = new GreeterA(new GreeterB(), new GreeterC(), new GreeterD(), new GreeterE());
+            greeterA.Greeting();
+        }
+    }
+    class Mediator
+    {
+        private GreeterB B { get; }
+        private GreeterC C { get; }
+        private GreeterD D { get; }
+        private GreeterE E { get; }
+
+        public Mediator(GreeterB b, GreeterC c, GreeterD d, GreeterE e)
+        {
+            B = b;
+            C = c;
+            D = d;
+            E = e;
+        }
+    }
+    class GreeterA
+    {
+        private GreeterB _b;
+        private GreeterC _c;
+        private GreeterD _d;
+        private GreeterE _e;
+
+        public GreeterA(GreeterB b, GreeterC c, GreeterD d, GreeterE e)
+        {
+            _b = b;
+            _c = c;
+            _d = d;
+            _e = e;
+        }
+        public void Greeting()
+        {
+            Console.WriteLine($"Hello {this._b.Name}");
+            Console.WriteLine($"Hello {this._c.Name}");
+            Console.WriteLine($"Hello {this._d.Name}");
+            Console.WriteLine($"Hello {this._e.Name}");
+        }
+    }
+    class GreeterB
+    {
+        public string Name { get; } = "B";
+    }
+    class GreeterC
+    {
+        public string Name { get; } = "C";
+    }
+    class GreeterD
+    {
+        public string Name { get; } = "D";
+    }
+    class GreeterE
+    {
+        public string Name { get; } = "E";
+    }
+}
---
+++ hello-app-cli-mediatr/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
+using Autofac;
+using MediatR;
+
+namespace hello_app_cli_mediatr;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterType<Mediator>().As<IMediator>();
+        builder.Register<ServiceFactory>(context =>
+        {
+            var c = context.Resolve<IComponentContext>();
+            return t => c.Resolve(t);
+        });
+        builder.RegisterType<PingHandler>().As<IRequestHandler<Ping, string>>();
+        var container = builder.Build();
+        var mediator = container.Resolve<IMediator>();
+        var response = mediator.Send(new Ping());
+        Console.WriteLine(response.Result);
+    }
+}
+
+public class Ping : IRequest<string> { }
+
+public class PingHandler : IRequestHandler<Ping, string>
+{
+    public Task<string> Handle(Ping request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult("Pong");
+    }
+}
